1e68a369f48055bf81ed578edb19353ddc0901b0
kim-sunah <1004_sunah@naver.com>
2025-01-22 18:49:54 +0900

    IceCandidate

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -8,19 +8,27 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set("views", __dirname + '/views');
 app.use("/public", express.static(__dirname + '/public'));
-app.get('/', (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.render("/"))
+app.get("/*", (_, res) => res.render("home"));
+// app.get("/*", (_, res) => res.render("/"))
 
 const server = http.createServer(app)
 const io = SocketIO(server)
 
 io.on("connection", (socket) => {
-    socket.on('join_room', (roomName, done) => {
+    socket.on('join_room', (roomName) => {
         socket.join(roomName)
-        done();
         socket.to(roomName).emit("welcome")
         // socket.broadcast.to(roomName).emit('user_connected', roomName)
     })
+    socket.on('offer', (offer, roomName) => {
+        socket.to(roomName).emit('offer', offer)
+    })
+    socket.on('answer', (answer, roomName) => {
+        socket.to(roomName).emit('answer', answer);
+    });
+    socket.on('ice', (ice, roomName) => {
+        socket.to(roomName).emit('ice', ice);
+    });
 })
 
 const Listen = () => console.log(`Listening on http://localhost:${port}`)
